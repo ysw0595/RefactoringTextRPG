97972852a2b611c05ba65b58dc4a86f5c974bcf4
ysw0595 <81400294+ysw0595@users.noreply.github.com>
2026-06-09 15:24:42 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TextRPG_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 게임 객체 생성
+            Game.Game game = new Game.Game();
+
+            // end가 false가 될 때까지 무한 루프 (FSM 기초)
+            while (game.End())
+            {
+                game.Process();
+            }
+        }
+    }
+}
